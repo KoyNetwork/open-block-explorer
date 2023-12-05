70b17651592c793057f3c61c294fa47bcd2eb0f2
Karyne Mayer <karyne@detroitledger.tech>
2023-12-04 16:46:09 -0800

    Replaces funemojis with custom avatars

---
+++ src/utils/koyn-avatar/components/face.js
@@ -0,0 +1,8 @@
+/**
+ * Do not change this file manually! This file was generated with the "Dicebear Exporter"-Plugin for Figma.
+ *
+ * Plugin: https://www.figma.com/community/plugin/1005765655729342787
+ */
+export const face = {
+    'square': (components, colors) => `<path fill="#F0C146" d="M0 0h512v512H0z"/>`,
+};
---
+++ src/utils/koyn-avatar/schema.js
@@ -0,0 +1,206 @@
+/**
+ * Do not change this file manually! This file was generated with the "Dicebear Exporter"-Plugin for Figma.
+ *
+ * Plugin: https://www.figma.com/community/plugin/1005765655729342787
+ */
+export const schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "properties": {
+        "eyes": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "enum": [
+                    "horns",
+                    "eyebrows6",
+                    "eyebrows5",
+                    "eyebrows4",
+                    "round",
+                    "sad",
+                    "relaxed",
+                    "dizzy",
+                    "winkWavy",
+                    "surprise",
+                    "dead",
+                    "babySad",
+                    "coldSweat",
+                    "wink",
+                    "questioning",
+                    "dots",
+                    "squinty",
+                    "happy",
+                    "eyepatch",
+                    "alien",
+                    "redLaser2",
+                    "glassesShade2",
+                    "glassesXl",
+                    "underwater",
+                    "winter",
+                    "sportGlasses",
+                    "glassesShadeRect",
+                    "squinting",
+                    "kOYN",
+                    "pinkGlasses2",
+                    "pinkGlasses",
+                    "euro",
+                    "dollarSign",
+                    "anim1",
+                    "btc1",
+                    "happy1",
+                    "redLaser",
+                    "baby",
+                    "btc2",
+                    "bigEyes",
+                    "bindiDot1",
+                    "bindiDot2"
+                ]
+            },
+            "default": [
+                "horns",
+                "eyebrows6",
+                "eyebrows5",
+                "eyebrows4",
+                "round",
+                "sad",
+                "relaxed",
+                "dizzy",
+                "winkWavy",
+                "surprise",
+                "dead",
+                "babySad",
+                "coldSweat",
+                "wink",
+                "questioning",
+                "dots",
+                "squinty",
+                "happy",
+                "eyepatch",
+                "alien",
+                "redLaser2",
+                "glassesShade2",
+                "glassesXl",
+                "underwater",
+                "winter",
+                "sportGlasses",
+                "glassesShadeRect",
+                "squinting",
+                "kOYN",
+                "pinkGlasses2",
+                "pinkGlasses",
+                "euro",
+                "dollarSign",
+                "anim1",
+                "btc1",
+                "happy1",
+                "redLaser",
+                "baby",
+                "btc2",
+                "bigEyes",
+                "bindiDot1",
+                "bindiDot2"
+            ]
+        },
+        "face": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "enum": [
+                    "square"
+                ]
+            },
+            "default": [
+                "square"
+            ]
+        },
+        "misc": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "enum": [
+                    "hands",
+                    "halo",
+                    "bandage"
+                ]
+            },
+            "default": [
+                "hands",
+                "halo",
+                "bandage"
+            ]
+        },
+        "miscProbability": {
+            "type": "integer",
+            "minimum": 0,
+            "maximum": 100,
+            "default": 8
+        },
+        "mouth": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "enum": [
+                    "fangs",
+                    "tongueUp",
+                    "sad1",
+                    "zipper",
+                    "wavy2",
+                    "wavy",
+                    "pursed",
+                    "teeth",
+                    "bigO",
+                    "smallO",
+                    "wideSmile2",
+                    "wideSmile",
+                    "tongue4",
+                    "largeChin",
+                    "oval",
+                    "smileLg",
+                    "meh2",
+                    "confounded",
+                    "happy",
+                    "sad2",
+                    "yen",
+                    "pound",
+                    "mildySad",
+                    "tongue1",
+                    "toxicMask",
+                    "meh",
+                    "tongue2",
+                    "smile1",
+                    "blush1"
+                ]
+            },
+            "default": [
+                "fangs",
+                "tongueUp",
+                "sad1",
+                "zipper",
+                "wavy2",
+                "wavy",
+                "pursed",
+                "teeth",
+                "bigO",
+                "smallO",
+                "wideSmile2",
+                "wideSmile",
+                "tongue4",
+                "largeChin",
+                "oval",
+                "smileLg",
+                "meh2",
+                "confounded",
+                "happy",
+                "sad2",
+                "yen",
+                "pound",
+                "mildySad",
+                "tongue1",
+                "toxicMask",
+                "meh",
+                "tongue2",
+                "smile1",
+                "blush1"
+            ]
+        }
+    }
+};
